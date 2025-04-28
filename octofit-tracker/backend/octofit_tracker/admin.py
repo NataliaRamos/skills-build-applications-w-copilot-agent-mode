713eff65d85ee1eb818f0e11efc8f721a1c4e502
Natalia Ramos <NataliaRamos@users.noreply.github.com>
2025-04-28 22:09:57 +0000

    Removed Team model registration from admin.py due to incompatibility with Django admin

--- octofit-tracker/backend/octofit_tracker/admin.py
+++ octofit-tracker/backend/octofit_tracker/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import User, Team, Activity, Leaderboard, Workout
 
 # admin.site.register(User)
-admin.site.register(Team)
+# Removed Team model registration as it is not compatible with Django admin
 admin.site.register(Activity)
 admin.site.register(Leaderboard)
 admin.site.register(Workout)
